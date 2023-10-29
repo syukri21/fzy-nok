9a4282e5961d99dd0bfad6004e8b217ca93544d5
syukri21 <syukrihsb148@gmail.com>
2023-10-29 22:39:43 +0700

    add ppic seeder

--- app/Models/PPICModel.php
+++ app/Models/PPICModel.php
@@ -20,39 +20,6 @@ class PPICModel extends UserModel
         $this->faker = Factory::create("id_ID");
     }
 
-    protected $DBGroup          = 'default';
-    protected $table            = 'ppics';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
-
     public function findRunningProductionById(int $ppic_id): \stdClass
     {
         $runningProduction = $this->db->query("
